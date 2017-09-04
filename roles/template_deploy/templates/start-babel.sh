17881fa507525655b02778388a31d7719b0b48c5
jkilpatr <jkilpatr@redhat.com>
2017-09-04 17:05:35 -0400

    Proxy all client traffic over wireguard
    With this addition we have what I would consider to be a full
    integration testing environment.
    
    Also added some documentation to the start babel script, which was
    starting to look more than a little messy and toggled the devel
    setting back to false by default, as it should be.

--- roles/template_deploy/templates/start-babel.sh
+++ roles/template_deploy/templates/start-babel.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 rm /var/run/babeld.pid
 set -eux
+# wlan config
 ifconfig wlan0 up
 iwconfig wlan0 mode ad-hoc
 iwconfig wlan0 essid "Althea-demo"
 iwconfig wlan0 channel {{channel}}
 iwconfig wlan0 txpower {{tx_power}}
+
+# gateway has a static external ip, everyone else is randomized
 {% if 'gateway' in group_names %}
 set +eux
 ip a add {{gateway_ip}}/16 dev wlan0
@@ -15,25 +18,36 @@ set +eux
 ip a add {{generated_ip.stdout}}/16 dev wlan0
 set -eux
 {% endif %}
+
+# if devel is enabled setup wireguard
 {% if devel %}
 set +eux
 ip link add dev wg0 type wireguard
-ip address add dev wg0 {{generatedv6_ip.stdout}}/128
-ip -6 a add fe80::/64 dev wg0
+# babel for some reason can't assign the link local ipv6 address itself on wg
+ip address add dev wg0 {{generatedv6_ip.stdout}}/64
 wg setconf wg0 /home/pi/wg.conf
 ip link set up dev wg0
 set -eux
 {% endif %}
-{% if 'client' in group_names %}
+
+# Client, setup add wg ipv4 address and ipv4 default route
+{% if 'client' in group_names and devel %}
 set +eux
-route del default
+ip a add 10.0.{{ 254 | random }}.{{ 254 | random }}/16 dev wg0
+ip route del default
 set -eux
-ip route add default via {{gateway_ip}} dev wlan0
+ip route add default via {{gateway_internal_ip}} dev wg0
 {% endif %}
-{% if 'gateway' in group_names %}
+
+# Gateway setup, setup nat, add gateway fixed wg internal ip
+{% if 'gateway' in group_names and devel %}
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
+iptables -A FORWARD -i eth0 -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i wg0 -o eth0 -j ACCEPT
+ip address add dev wg0 {{gateway_internal_ip}}/16
 {% endif %}
+
+# Run babel with a price of 1024, a management server on 8080, on the wireguard
+# and wlan interfaces with a hello period of 1 only advertising routes on wlan0
 babeld -d 1 -h 1 -P 1024 -G 8080 -w wlan0 {% if devel %}wg0{% endif %} \
 -C 'redistribute local if wlan0' -C 'redistribute local deny'
