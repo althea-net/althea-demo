277c0fb77c3c3b2cc8c7a6c2e815f0068f871183
Jehan <jehan.tremback@gmail.com>
2017-09-14 14:21:01 -0700

    traffic generator changes

--- roles/traffic_gen/templates/traffic-generator.sh
+++ roles/traffic_gen/templates/traffic-generator.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-pushd $HOME
+cd $HOME
 {% if 'gateway' in group_names %}
 while true; do
-	nc -u -l 7777 > junk.file
+	nc -u -l 7777 > /dev/null
 done
 {% elif  'client' in group_names %}
 dd if=/dev/urandom of=out.file count=10000
 while true; do
-	cat out.file | nc -u {{gateway_ip}} 7777
+	cat /dev/urandom | nc -u 10.28.7.7 7777
+	sleep 0.1
 done
 {% endif %}
