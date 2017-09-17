3e4eb765dc3fb5f9f1a93f6a5cd4a1a015c45d2f
Jehan <jehan.tremback@gmail.com>
2017-09-17 15:46:49 -0700

    exorcise the devel

--- roles/template_deploy/templates/start-babel.sh
+++ roles/template_deploy/templates/start-babel.sh
@@ -19,37 +19,10 @@ ip a add {{generated_ip.stdout}}/16 dev wlan0
 set -eux
 {% endif %}
 
-# if devel is enabled setup wireguard
-{% if devel %}
-set +eux
-ip link add dev wg0 type wireguard
-# babel for some reason can't assign the link local ipv6 address itself on wg
-ip address add dev wg0 {{generatedv6_ip.stdout}}/64
-wg setconf wg0 /home/pi/wg.conf
-ip link set up dev wg0
-set -eux
-{% endif %}
-
-# Client, setup add wg ipv4 address and ipv4 default route
-{% if 'client' in group_names and devel %}
-set +eux
-ip a add 10.0.{{ 254 | random }}.{{ 254 | random }}/16 dev wg0
-ip route del default
-set -eux
-ip route add default via {{gateway_internal_ip}} dev wg0
-{% endif %}
-
-# Gateway setup, setup nat, add gateway fixed wg internal ip
-{% if 'gateway' in group_names and devel %}
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -A FORWARD -i eth0 -o wg0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i wg0 -o eth0 -j ACCEPT
-ip address add dev wg0 {{gateway_internal_ip}}/16
-{% endif %}
 
 # Run babel with a price of 1024, a management server on 8080, on the wireguard
 # and wlan interfaces with a hello period of 1 only advertising routes on wlan0
-babeld -d 1 -h 1 -P 1024 -G 8080 -w wlan0 {% if devel %}wg0{% endif %} \
+babeld -d 1 -h 1 -P 1024 -G 8080 -w wlan0 \
 -C "in if wg0 deny" \
 {% if 'client' in group_names %}
 {% for gateway in groups['gateway'] %}
