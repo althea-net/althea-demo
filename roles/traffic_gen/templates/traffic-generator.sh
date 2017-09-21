7944aa51e6b3ade6073dee0bf6e1a8057079781f
Jehan <jehan.tremback@gmail.com>
2017-09-20 18:35:16 -0700

    fix traffic gen script

--- roles/traffic_gen/templates/traffic-generator.sh
+++ roles/traffic_gen/templates/traffic-generator.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 cd "$HOME" || exit
-
-# {% if 'gateway' in group_names %}
-send_and_receive "{{client_mesh_ip}}"
-# {% elif  'client' in group_names %}
-send_and_receive "{{gateway_mesh_ip}}"
-# {% endif %}
-
-function send_and_receive() {
-	nc -u -l "{{traffic_gen_port}}" > /dev/null
+ 
+function send_and_receive {
+	nc -u -l "{{traffic_gen_port}}" > /dev/null &
 	while true; do
 		nc -u "$1" "{{traffic_gen_port}}" < /dev/urandom
 		sleep 0.1
 	done
 }
+
+# {% if 'gateway' in group_names %}
+
+send_and_receive "{{client_mesh_ip}}"
+
+# {% elif  'client' in group_names %}
+
+send_and_receive "{{gateway_mesh_ip}}"
+
+# {% endif %}
