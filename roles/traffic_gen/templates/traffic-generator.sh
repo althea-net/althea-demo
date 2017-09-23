890702270195d561caef8b2bcf71e689b81703bf
Jehan <jehan.tremback@gmail.com>
2017-09-23 13:33:11 -0700

    last minute demo tweaks

--- roles/traffic_gen/templates/traffic-generator.sh
+++ roles/traffic_gen/templates/traffic-generator.sh
@@ -1,37 +1,37 @@
-#!/bin/bash
-function send_and_receive {
-	nc -u -l "{{traffic_gen_port}}" > /dev/null &
-	while true; do
-		nc -u "$1" "{{traffic_gen_port}}" < /dev/urandom
-		sleep 1
-	done
-}
-
-# {% if 'gateway' in group_names %}
-
-send_and_receive "{{client_mesh_ip}}"
-
-# {% elif  'client' in group_names %}
-
-send_and_receive "{{gateway_mesh_ip}}"
-
-# {% endif %}
-
-
 # #!/bin/bash
-
 # function send_and_receive {
+# 	nc -u -l "{{traffic_gen_port}}" > /dev/null &
 # 	while true; do
-# 		sendip -p ipv4 -is "$1" -p udp -us "{{traffic_gen_port}}" -ud "{{traffic_gen_port}}" -d r1400 "$2"
+# 		nc -u "$1" "{{traffic_gen_port}}" < /dev/urandom
+# 		sleep 1
 # 	done
 # }
 
 # # {% if 'gateway' in group_names %}
 
-# send_and_receive "{{gateway_mesh_ip}}" "{{client_mesh_ip}}" 
+# send_and_receive "{{client_mesh_ip}}"
 
 # # {% elif  'client' in group_names %}
 
-# send_and_receive "{{client_mesh_ip}}" "{{gateway_mesh_ip}}"
+# send_and_receive "{{gateway_mesh_ip}}"
 
 # # {% endif %}
+
+
+#!/bin/bash
+
+function send_and_receive {
+	while true; do
+		sendip -p ipv4 -is "$1" -p udp -us "{{traffic_gen_port}}" -ud "{{traffic_gen_port}}" -d r1400 "$2"
+	done
+}
+
+# {% if 'gateway' in group_names %}
+
+send_and_receive "{{gateway_mesh_ip}}" "{{client_mesh_ip}}" 
+
+# {% elif  'client' in group_names %}
+
+send_and_receive "{{client_mesh_ip}}" "{{gateway_mesh_ip}}"
+
+# {% endif %}
